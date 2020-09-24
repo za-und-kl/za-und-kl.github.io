705929dc591eafd75424574548c337c72874268b
Alwus <execrafter44@gmail.com>
2020-09-24 14:35:52 +0200

    worte saome code

--- Bugs/main.js
+++ Bugs/main.js
@@ -1,8 +1,8 @@
 var bugs = [];
 
 function setup() {
-	createCanvas(200, 200);
-	bugs.push(new Bug(100, 100));
+	createCanvas(400, 400);
+	bugs.push(new Bug(200, 200));
 }
 
 function draw() {
@@ -18,18 +18,18 @@ function render() {
 }
 
 class Bug {
-	constructor(sx, sy) {
-		this.pos = createVector(sx, sy);
+	constructor(x, s) {
+		this.pos = createVector(x, y);
 		this.dir = createVector(1, 1);
 		this.dirs = [0, 60, 120, 180, -120, -60];	//vw, r, sr, rw, sl, l
-		this.dna = [1, 1, 1, 1, 1, 1];
+		this.dna = [10, 10, 10, 10, 10, 10];
 	}
-	
+
 	show() {
 		strokeWeight(3);
 		point(this.pos.x, this.pos.y);
 	}
-	
+
 	move() {
 		let vals = 0;
 		let map = [];
@@ -39,9 +39,15 @@ class Bug {
 				map.push(d);
 			}
 		}
-		print(map);	
+		print(map);
 		let r = random(map);
 		this.dir.rotate(radians(this.dirs[r]));
 		this.pos.add(this.dir);
+	}
+}
+
+class Food {
+	constructor(x) {
+
 	}
 }
